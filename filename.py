498aeaf6c51ac4c989c92e305965946c9f8eec53
Abhishek <108791816+Abhi-shek03@users.noreply.github.com>
2023-02-06 23:59:23 +0530

    Delete filename.py

--- filename.py
+++
@@ -1,5 +0,0 @@
-# Write a Python program to accept a filename from the user and print the extension of that.
-
-filename = input('Enter the file name: ')
-extension = filename.split('.')
-print(f"The extension of file is: '{extension[-1]}'")
